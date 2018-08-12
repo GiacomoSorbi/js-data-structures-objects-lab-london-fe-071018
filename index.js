50f7327a6af166de7f80121bb7fbc1b54fc35d8b
Giacomo Sorbi <g.sorbi@gmail.com>
2018-08-12 12:32:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Write your solution in this file!
 const updateDriverWithKeyAndValue=(v,k)=>Object.assign(drivers,{[v]: k});
+const destructivelyUpdateDriverWithKeyAndValue=(v,k)=>drivers[v]=k;
 const 
