f9d2849a602577ca08e06e1d3cca8bd4f9661fe4
Giacomo Sorbi <g.sorbi@gmail.com>
2018-08-12 12:40:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 const updateDriverWithKeyAndValue=(driver,v,k)=>Object.assign(drivers,{[v]: k});
 const destructivelyUpdateDriverWithKeyAndValue=(driver,v,k)=>drivers[v]=k;
 const deleteFromDriverByKey=(driver,v,k)=>delete drivers[v];
-const 
+const deleteFromDriverByKey=(driver,v,k)=>delete drivers[v];
